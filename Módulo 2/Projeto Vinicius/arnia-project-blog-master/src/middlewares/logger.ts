a040dbfe87abc9292234cc2baee63246e2ffb8c5
Mozar Lima <89784637+mozar86@users.noreply.github.com>
2024-04-07 22:50:47 -0300

    update 07-04-2024.2

--- Módulo 2/Projeto Vinicius/arnia-project-blog-master/src/middlewares/logger.ts
+++ Módulo 2/Projeto Vinicius/arnia-project-blog-master/src/middlewares/logger.ts
@@ -7,4 +7,4 @@ export function logger(request: Request, response: Response, next: NextFunction)
     console.log(`Date: ${new Date()}`);
     console.log("====================");
     next();
-}
+};
